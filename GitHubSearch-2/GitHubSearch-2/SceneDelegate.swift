cceb680955fa4bc7a09555213e1b7bb156596b67
Krzysztof Lema <lema10@o2.pl>
2024-05-18 09:35:50 +0200

    feat(GHS-3): add swiftlint to the project (#2)

--- GitHubSearch-2/GitHubSearch-2/SceneDelegate.swift
+++ GitHubSearch-2/GitHubSearch-2/SceneDelegate.swift
@@ -11,13 +11,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+        
         window = UIWindow(windowScene: windowScene)
         let viewController = ViewController()
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
 }
-
