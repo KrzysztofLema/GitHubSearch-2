7211bec79f5ccb11f88fd40d7a357f96459fae4d
Krzysztof Lema <lema10@o2.pl>
2024-06-04 17:47:40 +0200

    feat(GHS-14)add-github-colors-store

---
+++ GitHubSearch-2/GitHubSearch-2/Core/Data/stores/GitHubLanguageColorsStore.swift
@@ -0,0 +1,51 @@
+//
+//  LanguageColorsStore.swift
+//  GitHubSearch-2
+//
+//  Created by Krzysztof Lema on 03/06/2024.
+//
+
+import UIKit
+
+protocol LanguageColorsStoreType {
+    func getColor(for language: String) -> UIColor?
+}
+
+public final class LanguageColorsStore: LanguageColorsStoreType {
+   
+    typealias GitHubLanguageColorsValuesDTO = [String: GitHubLanguageColorsDTO]
+    typealias GitHubLanguageColorsValues = [String: GitHubLanguageColors]
+    
+    private var colors: GitHubLanguageColorsValues?
+    private let decoder = JSONDecoder()
+    
+    init() {
+        self.colors = loadJSONValues()
+    }
+    
+    func getColor(for language: String) -> UIColor? {
+        colors?[language]?.color
+    }
+    
+    private func loadJSONValues() -> GitHubLanguageColorsValues {
+        guard let url = Bundle.main.url(forResource: "GitHubLanguageColors", withExtension: "json"),
+              let data = try? Data(contentsOf: url),
+              let colors = try? decoder.decode(GitHubLanguageColorsValuesDTO.self, from: data) else {
+            return [:]
+        }
+        
+       return colors.mapValues({ GitHubLanguageColors(dto: $0) })
+    }
+    
+}
+
+struct LanguageColorsStoreKey: InjectionKey {
+    static var currentValue: LanguageColorsStoreType = LanguageColorsStore()
+}
+
+extension InjectedValues {
+    var languageColorStore: LanguageColorsStoreType {
+        get { Self[LanguageColorsStoreKey.self] }
+        set { Self[LanguageColorsStoreKey.self] = newValue }
+    }
+}
