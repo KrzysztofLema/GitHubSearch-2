ac6b7ebbcacce9013a8d7b39d7b54cfca7cdebc7
Krzysztof Lema <lema10@o2.pl>
2024-06-05 20:02:24 +0200

    feat(GHS-17): add firebase to project

--- GitHubSearch-2/GitHubSearch-2/AppDelegate.swift
+++ GitHubSearch-2/GitHubSearch-2/AppDelegate.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import CocoaLumberjackSwift
+import FirebaseCore
+import FirebaseAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         DDLog.add(DDOSLogger.sharedInstance)
+        FirebaseApp.configure()
         return true
     }
 
