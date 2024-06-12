76afc1ff60a56603bec8556fc3b5ab838e278978
Krzysztof Lema <lema10@o2.pl>
2024-06-12 11:26:21 +0200

    feat(GHS-21): Clean project from constants

---
+++ GitHubSearch-2/GitHubSearch-2/CoreUI/Style/UIConstants.swift
@@ -0,0 +1,23 @@
+//
+//  UIConstants.swift
+//  GitHubSearch-2
+//
+//  Created by Krzysztof Lema on 12/06/2024.
+//
+
+import Foundation
+
+struct UIConstants {
+    static let defaultCornerRadius = 12.0
+    static let defaultSpacing = 3.0
+    static let defaultImageSize = CGSize(width: 30, height: 30)
+    static let loginLogoImageSize = CGSize(width: 80, height: 80)
+    static let defaultLoginOffset = 20.0
+    static let defaultWidthMultiplier = 0.8
+    
+    struct Image {
+        static let logoImageViewTitle = "beach.umbrella.fill"
+        static let emailIconImageTitle = "at"
+        static let passwordIconImageTitle = "lock.circle"
+    }
+}
