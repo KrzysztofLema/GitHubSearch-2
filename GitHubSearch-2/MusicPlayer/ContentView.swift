489308b7d16f168b7f2b2ec865efed45f57203b3
Krzysztof Lema <lema10@o2.pl>
2024-06-20 07:33:42 +0200

    feat(GHS-31): Add Music player app to project to handle Music player framework.

---
+++ GitHubSearch-2/MusicPlayer/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MusicPlayer
+//
+//  Created by Krzysztof Lema on 19/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ GitHubSearch-2/MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Krzysztof Lema on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
