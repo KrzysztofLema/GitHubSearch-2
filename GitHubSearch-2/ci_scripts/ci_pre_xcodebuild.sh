d8dfd3d209074fb8db16022853c50e27a7bf6d8c
Krzysztof Lema <lema10@o2.pl>
2024-06-27 07:24:49 +0200

    bug(GHS-31): Add URL schema to configuration file

--- GitHubSearch-2/ci_scripts/ci_pre_xcodebuild.sh
+++ GitHubSearch-2/ci_scripts/ci_pre_xcodebuild.sh
@@ -2,5 +2,6 @@
 
 echo "Decoding GoogleService-Info.plist from environment variable..."
 echo "$GOOGLE_INFO_PLIST" | base64 --decode > ../GitHubSearch-2/SupportingFiles/GoogleService-Info.plist
+echo "$DEV_CONFIGURATION" | base64 --decode > ../GitHubSearch-2/SupportingFiles/DevConfiguration.xcconfig
 
 echo "Stage: PRE-Xcode Build is DONE .... "
